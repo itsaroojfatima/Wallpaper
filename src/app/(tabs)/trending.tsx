e56e1303f5f2ee380cacb53a8a7fd92140f37710
Arooj Fatima <aroojfatimapk388@gmail.com>
2026-08-26 19:46:28 +0500

    Create trending.tsx

---
+++ src/app/(tabs)/trending.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function TrendingScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Trending Wallpapers</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: "center", alignItems: "center" },
+});
