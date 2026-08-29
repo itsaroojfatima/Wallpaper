dffa0a7b6a13dd197ff799166cd5fb2c1fd42ab5
Muhammad Usman Tariq <142773789+usmanjutt47@users.noreply.github.com>
2026-08-29 09:05:23 +0500

    Create TrendingComponent.tsx

---
+++ components/TrendingComponent.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function TrendingComponent() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titleText}>Trending Screen Active</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: "100%",
+    flex: 1,
+    backgroundColor: "#701A75", // Distinct Magenta / Fuchsia
+    padding: 24,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  titleText: {
+    color: "#ffffff",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
